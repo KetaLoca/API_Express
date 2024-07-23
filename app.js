60d6941f5ed14f2d7698ad22f01ff48155acbb16
ketaloca <xesuspb@gmail.com>
2024-07-23 14:16:50 +0200

    validaciones hechas con zod

--- app.js
+++ app.js
@@ -28,6 +28,24 @@ app.get("/movies/:id", (req, res) => {
 });
 
 app.post("/movies", (req, res) => {
+  const movieSchema = z.object({
+    title: z.string({
+      invalid_type_error: 'Movie title must be a String',
+      required_error: 'Movie title is required'
+    }),
+    year: z.number().int().min(1900).max(2024),
+    director: z.string(),
+    rate: z.number().min(0).max(10),
+    poster: z.string().url({ message: 'Poster must be an URL' }),
+    genre: z.array(
+      z.enum(['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']),
+      {
+        invalid_type_error: 'This must be an Array',
+        required_error: 'Movie genre is required'
+      }
+    )
+  })
+
   const { title, genre, year, director, duration, rate, poster } = req.body;
 
   if (!title || !genre || !year || !director || !duration || !rate || !poster) {
