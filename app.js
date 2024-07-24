f43cb89dccb5c16029507688588e1369db00a3fc
ketaloca <xesuspb@gmail.com>
2024-07-24 11:00:23 +0200

    PATCH funcionando para cambiar el parámetro "year"

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const crypto = require("node:crypto");
 const movies = require("./movies.json");
-const { validateMovie } = require("./schemas/movies");
+const { validateMovie, validatePartialMovie } = require("./schemas/movies");
 
 const app = express();
 app.disable("x-powered-by");
@@ -62,12 +62,23 @@ app.post("/movies", (req, res) => {
 });
 
 app.patch("/movies/:id", (req, res) => {
+  const result = validatePartialMovie(req.body)
+
+  if (result.error) { return res.status(400).json(result.error.message) }
+
   const { id } = req.params
   const movieIndex = movies.findIndex(movie => movie.id == id)
 
   if (movieIndex == -1) { return res.status(404).json({ message: 'Movie not found' }) }
 
-  const movie = movies[movieIndex]
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+
+  return res.json(updateMovie)
 })
 
 const PORT = process.env.PORT ?? 1234;
--- schemas/movies.js
+++ schemas/movies.js
@@ -22,6 +22,10 @@ function validateMovie(object) {
     return movieSchema.safeParse(object)
 }
 
+function validatePartialMovie(object) {
+    return movieSchema.partial().safeParse(object)
+}
+
 module.exports = {
-    validateMovie
+    validateMovie, validatePartialMovie
 }
