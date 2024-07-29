3b168ee3b5e7df415acd8a945159c3bebbefe251
KetaLoca <mianramar@gmail.com>
2024-07-29 09:54:16 +0200

    route para movies funcionando

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express, { json } from "express";
-import { randomUUID } from "node:crypto";
 import cors from "cors";
+import { moviesRouter } from "./routes/movies.js";
 
 const app = express();
 app.disable("x-powered-by");
@@ -8,94 +8,7 @@ app.disable("x-powered-by");
 app.use(json());
 app.use(cors());
 
-app.get("/movies", (req, res) => {
-  const { genre } = req.query;
-  if (genre) {
-    const filteredMovies = movies.filter((movie) =>
-      movie.genre.includes(genre)
-    );
-    return res.json(filteredMovies);
-  }
-  res.json(movies);
-});
-
-app.get("/movies/:id", (req, res) => {
-  const { id } = req.params;
-  const movie = movies.find((movie) => movie.id == id);
-  if (movie) return res.json(movie);
-
-  res.status(404).json({ message: "movie not found" });
-});
-
-app.post("/movies", (req, res) => {
-  // const { title, genre, year, director, duration, rate, poster } = req.body;
-
-  // if (!title || !genre || !year || !director || !duration || !rate || !poster) {
-  //   return res.status(400).json({ message: "hay algún campo vacío" })
-  // }
-
-  // const movie = {
-  //   id: crypto.randomUUID(),
-  //   title,
-  //   genre,
-  //   year,
-  //   director,
-  //   duration,
-  //   rate: rate ?? 0,
-  //   poster,
-  // };
-
-  const result = validateMovie(req.body);
-
-  if (result.error) {
-    return res.status(400).json({ message: JSON.parse(result.error.message) });
-  }
-
-  const movie = {
-    id: randomUUID(),
-    ...result.data,
-  };
-
-  movies.push(movie);
-
-  res.status(201).json(movie);
-});
-
-app.patch("/movies/:id", (req, res) => {
-  const result = validatePartialMovie(req.body);
-
-  if (result.error) {
-    return res.status(400).json(result.error.message);
-  }
-
-  const { id } = req.params;
-  const movieIndex = movies.findIndex((movie) => movie.id == id);
-
-  if (movieIndex == -1) {
-    return res.status(404).json({ message: "Movie not found" });
-  }
-
-  const updateMovie = {
-    ...movies[movieIndex],
-    ...result.data,
-  };
-
-  movies[movieIndex] = updateMovie;
-
-  return res.json(updateMovie);
-});
-
-app.delete("/movies/:id", (req, res) => {
-  const { id } = req.params;
-  const movieIndex = movies.findIndex((movie) => movie.id == id);
-
-  if (movieIndex == -1)
-    return res.status(404).json({ message: "Movie not found" });
-
-  movies.slice(movieIndex, 1);
-
-  return res.json({ message: "Movie deleted" });
-});
+app.use("/movies", moviesRouter);
 
 const PORT = process.env.PORT ?? 1234;
 
--- routes/movies.js
+++ routes/movies.js
@@ -1,23 +1,26 @@
-import { json, Router } from "express";
-import { validateMovie, validatePartialMovie } from "./schemas/movies.js";
+import { Router } from "express";
+import { validateMovie, validatePartialMovie } from "../schemas/movies.js";
 import { createRequire } from "node:module";
 import { randomUUID } from "node:crypto";
 const require = createRequire(import.meta.url);
-const movies = require("./movies.json");
-const router = Router();
+const movies = require("../movies.json");
 
-router.get("/", (req, res) => {
+export const moviesRouter = Router();
+
+moviesRouter.get("/", (req, res) => {
   const { genre } = req.query;
   if (genre) {
     const filteredMovies = movies.filter((movie) =>
-      movie.genre.some((g) => g.toLowerCase() == genre.toLowerCase())
+      movie.genre.some(
+        (g) => g.toLocaleLowerCase() == genre.toLocaleLowerCase()
+      )
     );
     return res.json(filteredMovies);
   }
   res.json(movies);
 });
 
-router.get("/:id", (req, res) => {
+moviesRouter.get("/:id", (req, res) => {
   const { id } = req.params;
   const movie = movies.find((movie) => movie.id == id);
   if (movie) {
@@ -26,7 +29,7 @@ router.get("/:id", (req, res) => {
   res.status(404).json({ message: "Movie not found" });
 });
 
-router.post("/", (req, res) => {
+moviesRouter.post("/", (req, res) => {
   const result = validateMovie(req.body);
   if (result.error) {
     return res.status(400).json({ message: JSON.parse(result.error.message) });
@@ -42,7 +45,7 @@ router.post("/", (req, res) => {
   res.status(201).json(movie);
 });
 
-router.patch("/:id", (req, res) => {
+moviesRouter.patch("/:id", (req, res) => {
   const result = validatePartialMovie(req.body);
   if (result.error) {
     return res.status(400).json(result.error.message);
@@ -53,12 +56,12 @@ router.patch("/:id", (req, res) => {
   if (MovieIndex == -1) {
     return res.status(404).json({ message: "Movie not found" });
   }
-  const updatedMovie = { ...movies[MovieIndex].data, ...result.data };
+  const updatedMovie = { ...movies[MovieIndex], ...result.data };
   movies[MovieIndex] = updatedMovie;
   res.json(updatedMovie);
 });
 
-router.delete("/:id", (req, res) => {
+moviesRouter.delete("/:id", (req, res) => {
   const { id } = req.params;
   const movieIndex = movies.findIndex((movie) => movie.id == id);
   if (movieIndex == -1) {
