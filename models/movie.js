d1836857a8c5372ddaaff681ca6438fb0a313b43
KetaLoca <mianramar@gmail.com>
2024-07-29 11:23:07 +0200

    Empezando a organizar MVC

---
+++ models/movie.js
@@ -0,0 +1,44 @@
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
+const movies = require("../movies.json");
+import { randomUUID } from "node:crypto";
+
+export class MovieModel {
+  static getAll = async ({ genre }) => {
+    if (genre) {
+      return movies.filter((movie) =>
+        movie.genre.some((g) => g.toLowerCase() == genre.toLowerCase())
+      );
+    }
+    return movies;
+  };
+
+  static async getById({ id }) {
+    const movie = movies.find((movie) => movie.id == id);
+    return movie;
+  }
+
+  static async create({ input }) {
+    const movie = { id: randomUUID(), ...input.data };
+    movies.push(movie);
+  }
+
+  static async delete({ id }) {
+    const movieIndex = movies.findIndex((movie) => movie.id == id);
+    if (movieIndex == -1) {
+      return false;
+    }
+    movies.splice(movieIndex, 1);
+    return true;
+  }
+
+  static async update({ id, input }) {
+    const MovieIndex = movies.findIndex((movie) => movie.id == id);
+    if (MovieIndex == -1) {
+      return false;
+    }
+    const updatedMovie = { ...movies[MovieIndex], ...result.data };
+    movies[MovieIndex] = updatedMovie;
+    return true;
+  }
+}
--- routes/movies.js
+++ routes/movies.js
@@ -1,73 +1,53 @@
 import { Router } from "express";
 import { validateMovie, validatePartialMovie } from "../schemas/movies.js";
-import { createRequire } from "node:module";
-import { randomUUID } from "node:crypto";
-const require = createRequire(import.meta.url);
-const movies = require("../movies.json");
+import { MovieModel } from "../models/movie.js";
 
 export const moviesRouter = Router();
 
-moviesRouter.get("/", (req, res) => {
+moviesRouter.get("/", async (req, res) => {
   const { genre } = req.query;
-  if (genre) {
-    const filteredMovies = movies.filter((movie) =>
-      movie.genre.some(
-        (g) => g.toLocaleLowerCase() == genre.toLocaleLowerCase()
-      )
-    );
-    return res.json(filteredMovies);
-  }
+  const movies = await MovieModel.getAll({ genre });
   res.json(movies);
 });
 
-moviesRouter.get("/:id", (req, res) => {
+moviesRouter.get("/:id", async (req, res) => {
   const { id } = req.params;
-  const movie = movies.find((movie) => movie.id == id);
+  const movie = await MovieModel.getById({ id });
   if (movie) {
     return res.json(movie);
   }
   res.status(404).json({ message: "Movie not found" });
 });
 
-moviesRouter.post("/", (req, res) => {
+moviesRouter.post("/", async (req, res) => {
   const result = validateMovie(req.body);
   if (result.error) {
     return res.status(400).json({ message: JSON.parse(result.error.message) });
   }
 
-  const movie = {
-    id: randomUUID(),
-    ...result.data,
-  };
-
-  movies.push(movie);
+  await MovieModel.create(result.data);
 
   res.status(201).json(movie);
 });
 
-moviesRouter.patch("/:id", (req, res) => {
+moviesRouter.patch("/:id", async (req, res) => {
   const result = validatePartialMovie(req.body);
   if (result.error) {
     return res.status(400).json(result.error.message);
   }
-
   const { id } = req.params;
-  const MovieIndex = movies.findIndex((movie) => movie.id == id);
-  if (MovieIndex == -1) {
-    return res.status(404).json({ message: "Movie not found" });
+  const comprobar = await MovieModel.update(id, result);
+  if (comprobar) {
+    return res.json({ message: "Película modificada correctamente" });
   }
-  const updatedMovie = { ...movies[MovieIndex], ...result.data };
-  movies[MovieIndex] = updatedMovie;
-  res.json(updatedMovie);
+  return res.json({ message: "Movie not found" });
 });
 
-moviesRouter.delete("/:id", (req, res) => {
+moviesRouter.delete("/:id", async (req, res) => {
   const { id } = req.params;
-  const movieIndex = movies.findIndex((movie) => movie.id == id);
-  if (movieIndex == -1) {
-    return res.status(404).json({ message: "Movie not found" });
+  const result = await MovieModel.delete(id);
+  if (result) {
+    return res.json({ message: "Movie deleted" });
   }
-
-  movies.splice(movieIndex, 1);
-  res.json({ message: "Movie deleted" });
+  res.json({ message: "Movie not found" });
 });
