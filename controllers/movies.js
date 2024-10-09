25ee7e668c39559569b39c4563d748ab35aa3f9d
ketaloca <xesuspb@gmail.com>
2024-10-09 15:25:58 +0200

    Extracción lógica controladores

---
+++ controllers/movies.js
@@ -0,0 +1,54 @@
+import { MovieModel } from "../models/movie";
+import { validateMovie, validatePartialMovie } from "../schemas/movies.js";
+import { MovieModel } from "../models/movie.js";
+
+export class MovieController {
+
+    static async getAll(req, res) {
+        const { genre } = req.query;
+        const movies = await MovieModel.getAll({ genre });
+        res.json(movies);
+    }
+
+    static async getById(req, res) {
+        const { id } = req.params;
+        const movie = await MovieModel.getById({ id });
+        if (movie) {
+            return res.json(movie);
+        }
+        res.status(404).json({ message: "Movie not found" });
+    }
+
+    static async create(req, res) {
+        const result = validateMovie(req.body);
+        if (result.error) {
+            return res.status(400).json({ message: JSON.parse(result.error.message) });
+        }
+
+        await MovieModel.create(result.data);
+
+        res.status(201).json(movie);
+    }
+
+    static async update(req, res) {
+        const result = validatePartialMovie(req.body);
+        if (result.error) {
+            return res.status(400).json(result.error.message);
+        }
+        const { id } = req.params;
+        const comprobar = await MovieModel.update(id, result);
+        if (comprobar) {
+            return res.json({ message: "Película modificada correctamente" });
+        }
+        return res.json({ message: "Movie not found" });
+    }
+
+    static async delete(req, res) {
+        const { id } = req.params;
+        const result = await MovieModel.delete(id);
+        if (result) {
+            return res.json({ message: "Movie deleted" });
+        }
+        res.json({ message: "Movie not found" });
+    }
+}
--- routes/movies.js
+++ routes/movies.js
@@ -1,53 +1,14 @@
 import { Router } from "express";
-import { validateMovie, validatePartialMovie } from "../schemas/movies.js";
-import { MovieModel } from "../models/movie.js";
+import { MovieController } from "../controllers/movies.js";
 
 export const moviesRouter = Router();
 
-moviesRouter.get("/", async (req, res) => {
-  const { genre } = req.query;
-  const movies = await MovieModel.getAll({ genre });
-  res.json(movies);
-});
+moviesRouter.get("/", MovieController.getAll);
 
-moviesRouter.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const movie = await MovieModel.getById({ id });
-  if (movie) {
-    return res.json(movie);
-  }
-  res.status(404).json({ message: "Movie not found" });
-});
+moviesRouter.get("/:id", MovieController.getById);
 
-moviesRouter.post("/", async (req, res) => {
-  const result = validateMovie(req.body);
-  if (result.error) {
-    return res.status(400).json({ message: JSON.parse(result.error.message) });
-  }
+moviesRouter.post("/", MovieController.create);
 
-  await MovieModel.create(result.data);
+moviesRouter.patch("/:id", MovieController.update);
 
-  res.status(201).json(movie);
-});
-
-moviesRouter.patch("/:id", async (req, res) => {
-  const result = validatePartialMovie(req.body);
-  if (result.error) {
-    return res.status(400).json(result.error.message);
-  }
-  const { id } = req.params;
-  const comprobar = await MovieModel.update(id, result);
-  if (comprobar) {
-    return res.json({ message: "Película modificada correctamente" });
-  }
-  return res.json({ message: "Movie not found" });
-});
-
-moviesRouter.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  const result = await MovieModel.delete(id);
-  if (result) {
-    return res.json({ message: "Movie deleted" });
-  }
-  res.json({ message: "Movie not found" });
-});
+moviesRouter.delete("/:id", MovieController.delete);
